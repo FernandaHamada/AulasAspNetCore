1a232a49ca8fcd85b270cd21484613e6d962e618
Fernanda Hamada <fernandakubayashi@gmail.com>
2019-11-02 23:31:30 -0300

    Add project files.

---
+++ Models/ErrorViewModel.cs
@@ -0,0 +1,11 @@
+using System;
+
+namespace AulasAspNetCore.Models
+{
+    public class ErrorViewModel
+    {
+        public string RequestId { get; set; }
+
+        public bool ShowRequestId => !string.IsNullOrEmpty(RequestId);
+    }
+}
