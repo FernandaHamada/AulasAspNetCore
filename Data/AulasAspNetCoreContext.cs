e89e9c39f0c5c872f36ba0b81d7ca4da7f08dbb1
Fernanda Hamada <fernandakubayashi@gmail.com>
2019-11-06 11:38:54 -0300

    CRUD Scaffolding

---
+++ Data/AulasAspNetCoreContext.cs
@@ -0,0 +1,18 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using Microsoft.EntityFrameworkCore;
+
+namespace AulasAspNetCore.Models
+{
+    public class AulasAspNetCoreContext : DbContext
+    {
+        public AulasAspNetCoreContext (DbContextOptions<AulasAspNetCoreContext> options)
+            : base(options)
+        {
+        }
+
+        public DbSet<AulasAspNetCore.Models.Department> Department { get; set; }
+    }
+}
--- Startup.cs
+++ Startup.cs
@@ -9,6 +9,8 @@ using Microsoft.AspNetCore.HttpsPolicy;
 using Microsoft.AspNetCore.Mvc;
 using Microsoft.Extensions.Configuration;
 using Microsoft.Extensions.DependencyInjection;
+using Microsoft.EntityFrameworkCore;
+using AulasAspNetCore.Models;
 
 namespace AulasAspNetCore
 {
@@ -33,6 +35,9 @@ namespace AulasAspNetCore
 
 
             services.AddMvc().SetCompatibilityVersion(CompatibilityVersion.Version_2_1);
+
+            services.AddDbContext<AulasAspNetCoreContext>(options =>
+                    options.UseSqlServer(Configuration.GetConnectionString("AulasAspNetCoreContext")));
         }
 
         // This method gets called by the runtime. Use this method to configure the HTTP request pipeline.
