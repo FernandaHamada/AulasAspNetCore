36708078b3aa19f75f2850a70fd9d8ad4b4c5e47
Fernanda Hamada <fernandakubayashi@gmail.com>
2019-11-16 14:11:53 -0300

    Update seller and custom service exception

--- Controllers/SellersController.cs
+++ Controllers/SellersController.cs
@@ -5,6 +5,7 @@ using System.Threading.Tasks;
 using AulasAspNetCore.Models;
 using AulasAspNetCore.Models.ViewModels;
 using AulasAspNetCore.Services;
+using AulasAspNetCore.Services.Exceptions;
 using Microsoft.AspNetCore.Mvc;
 
 namespace AulasAspNetCore.Controllers
@@ -47,7 +48,7 @@ namespace AulasAspNetCore.Controllers
             }
 
             var obj = _sellerService.FindById(id.Value);
-            if(obj == null)
+            if (obj == null)
             {
                 return NotFound();
             }
@@ -77,5 +78,46 @@ namespace AulasAspNetCore.Controllers
 
             return View(obj);
         }
+
+        public IActionResult Edit(int? id)
+        {
+            if (id == null)
+            {
+                return NotFound();
+            }
+
+            var obj = _sellerService.FindById(id.Value);
+            if (obj == null)
+            {
+                return NotFound();
+            }
+
+            List<Department> departments = _departmentService.FindAll();
+            SellerFormViewModel viewModel = new SellerFormViewModel { Seller = obj, Departments = departments };
+            return View(viewModel);
+        }
+        [HttpPost]
+        [AutoValidateAntiforgeryToken]
+        public IActionResult Edit(int id, Seller seller)
+        {
+            if (id != seller.Id)
+            {
+                return BadRequest();
+            }
+            try
+            {
+                _sellerService.Update(seller);
+                return RedirectToAction(nameof(Index));
+            }
+            catch (NotFoundException)
+            {
+                return NotFound();
+            }
+            catch (DbConcurrencyException)
+            {
+                return BadRequest();
+            }
+        }
+    
     }
 }
