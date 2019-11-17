ea08bbc024b36f41a1449ae6ff3360d4a47aece3
Fernanda Hamada <fernandakubayashi@gmail.com>
2019-11-16 23:36:22 -0300

    Exception handling for delete (referential integrity)

--- Services/SellerService.cs
+++ Services/SellerService.cs
@@ -25,7 +25,7 @@ namespace AulasAspNetCore.Services
         public async Task InsertAsync(Seller obj)
         {
             _context.Add(obj);
-           await _context.SaveChangesAsync();
+            await _context.SaveChangesAsync();
         }
 
         public async Task<Seller> FindByIdAsync(int id)
@@ -35,10 +35,18 @@ namespace AulasAspNetCore.Services
 
         public async Task RemoveAsync(int id)
         {
-            Seller obj = await _context.Seller.FindAsync(id);
-            _context.Seller.Remove(obj);
-            await _context.SaveChangesAsync();
+            try
+            {
+                Seller obj = await _context.Seller.FindAsync(id);
+                _context.Seller.Remove(obj);
+                await _context.SaveChangesAsync();
+            }
+            catch (DbUpdateException e)
+            {
+                throw new IntegrityException("Can't delete seller because he/she has sales");
+            }
         }
+
 
         public async Task UpdateAsync(Seller obj)
         {
